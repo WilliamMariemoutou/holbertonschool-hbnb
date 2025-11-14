3c06d97241a81d49488171f2cfa0947185457d21
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-11-15 00:23:46 +0400

    Base model

---
+++ part2/hbnb/app/models/base_model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """A simple base class that other models will inherit from"""
+    
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        
+    def save(self):
+        """update the updated_at timestamp"""
+        self.updated_at = datetime.now()
+        
+    def update(self, data):
+        """Update values using a dictionary"""
+        for key, value in data.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        self.save()
